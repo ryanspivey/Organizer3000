e0f77d4b9f79177eb4ddc77685eb5f70225b2068
Ryan L Spivey <36359054+ryanspivey@users.noreply.github.com>
2018-12-08 17:08:44 -0600

    Category.java
    This is a constructor to save the category name selected from a JList component. I don't think this is the most appropriate way of doing this; however, it works for the time being...

---
+++ Category.java
@@ -0,0 +1,16 @@
+package com.Organizer3000.eclipse.ide.first;
+
+public class Category {
+    private String fileName;
+    
+    public Category(String fileName){
+        this.fileName=fileName;
+        
+    }  
+    
+    public String toString() {
+        return fileName;
+    }
+    
+    
+}
